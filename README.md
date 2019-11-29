b1c06fdd6db3d8fc7fdd857c9409860d2b1b917c
yangtz <yangtzl@foxmail.com>
2019-11-29 14:50:44 +0800

    add 整合mybatis和log4j2日志

---
+++ README.md
@@ -0,0 +1,4 @@
+### 整合Mybatis
+##### Mybatis自动建表
+##### Mybatis自动生成mapper文件
+### 整合log4j2日志
--- core/src/main/java/cn/yang/CoreApplication.java
+++ core/src/main/java/cn/yang/CoreApplication.java
@@ -1,13 +1,18 @@
-package cn.yang.core;
+package cn.yang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "cn.yang.*")
+@MapperScan(value = "cn.yang.dao")
 public class CoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
+        System.out.println("====================秒杀项目启动完毕=================");
     }
 
 }
